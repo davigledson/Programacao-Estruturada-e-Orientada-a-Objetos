fcdc89914212b83697a1e3d652717be4c2e203ef
davigledson <davigledson@outlook.com>
2023-01-18 02:09:46 -0300

    Update ex006_classMoto.py

--- Orientação A Objetos/Lista de Exercicio sobre Herança/ex006_classMoto.py
+++ Orientação A Objetos/Lista de Exercicio sobre Herança/ex006_classMoto.py
@@ -1,10 +1,19 @@
 from ex004_classTransporte import Transporte
 class Moto(Transporte):
 
-    def __init__(self, fabricante, ano, modelo, cilindrada):
+    def __init__(self, fabricante, ano, modelo, cilindradas):
         super().__init__(fabricante, ano, modelo)
-        self.cilindrada = cilindrada
+        self.cilindradas = cilindradas
 
+    def empinarRoda(self):
+        print('Roda dianteira foi erguida')
 
+    def info(self):
+        super().info()
+        print('Cilindradas:',self.cilindradas)
+
+moto1=Moto('Honda',2020,'novo',3000)
+moto1.info()
+moto1.empinarRoda()
 
 
